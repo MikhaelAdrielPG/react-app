8e73a8fc5282446345795223c3819da5a08ecf1f
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-16 22:12:28 +0700

    JSX

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,22 @@
 function Article() {
-  return <div>Init adalah component pertama, dan kedua</div>;
+  const name = "Mikhael";
+  const titles = ["Tutorial Reactjs", "Tutorial Nextjs", "Tutorial Nodejs"];
+
+  return (
+    <>
+      <div>{name}</div>
+      <div>
+        {titles.map((title) => {
+          return (
+            <>
+              <div>{title}</div>
+              <div>{title}</div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Article;
