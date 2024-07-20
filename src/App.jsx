b3a6d1780286ea89e994ab072ae2e19e347fd43e
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-20 09:19:41 +0700

    Conditional Rendering 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import "./App.css";
 import Home from "./pages/index";
 
 function App() {
-  const value = true;
-
   return (
     <div className="App">
-      {value ? "benar" : "salah"}
       <Home />
     </div>
   );
