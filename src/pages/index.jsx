8a2de49da2274dc6dc2abeb18c1a8d8a1ad707d8
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-20 09:03:53 +0700

    Mengembalikan Value ke Parent melalui Props

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,24 @@
-import posts from "../posts.json";
+import postsData from "../posts.json";
 import Article from "../components/Article";
+import Search from "../components/Search";
 import { useState } from "react";
 
 function HomePage() {
-  const [search, setSearch] = useState("");
+  const [posts, setPosts] = useState(postsData);
 
-  const changeSearch = (event) => {
-    setSearch(event.target.value);
+  const onSearchChange = (value) => {
+    const filteredPosts = postsData.filter((item) =>
+      item.title.includes(value)
+    );
+    setPosts(filteredPosts);
   };
 
   return (
     <>
       <h1>Simple Blog</h1>
-      <div>
-        Cari Artikel : <input onChange={changeSearch}></input>{" "}
-      </div>
-      <small>Ditermukan 0 data dengan pencarian kata {search}</small>
-      {posts.map(({ title, tags, date }, index) => (
-        <Article {...{ title, tags, date }} key={index} />
+      <Search onSearchChange={onSearchChange} />
+      {posts.map((props, index) => (
+        <Article {...props} key={index} />
       ))}
     </>
   );
